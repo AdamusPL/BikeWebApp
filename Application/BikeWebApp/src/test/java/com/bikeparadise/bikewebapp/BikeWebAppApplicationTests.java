29dbcd1fdb7ef18580041c8127e6de8ca120b6a6
AdamusPL <adam.czekalski.szczecin@gmail.com>
2024-08-20 19:54:56 +0200

    application init

---
+++ Application/BikeWebApp/src/test/java/com/bikeparadise/bikewebapp/BikeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikeparadise.bikewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
