aef21c680f79c8bd3a83d2bf4918784c610ba86f
AdamusPL <adam.czekalski.szczecin@gmail.com>
2024-08-20 20:43:40 +0200

    tables creation in database
    + small diagram fix + helpful queries

---
+++ Database/Queries/AAAA_DROP_TABLES.sql
@@ -0,0 +1,12 @@
+USE BikeShop
+GO;
+
+DROP TABLE BikeShop.dbo.[Review];
+DROP TABLE BikeShop.dbo.[Part];
+DROP TABLE BikeShop.dbo.[Bike];
+DROP TABLE BikeShop.dbo.[Order];
+DROP TABLE BikeShop.dbo.[Client];
+DROP TABLE BikeShop.dbo.[ShopAssistant];
+DROP TABLE BikeShop.dbo.[Contact];
+DROP TABLE BikeShop.dbo.[Data];
+DROP TABLE BikeShop.dbo.[User];
---
+++ Database/Queries/BBBB_CHECKING_SELECTS.sql
@@ -0,0 +1,19 @@
+USE BikeShop;
+GO
+
+SELECT * FROM [User];
+SELECT * FROM [Contact];
+SELECT * FROM [Data];
+SELECT * FROM [ShopAssistant];
+SELECT * FROM [Client];
+SELECT * FROM [Order];
+SELECT * FROM [Bike];
+SELECT * FROM [Part];
+SELECT * FROM [Review];
+SELECT * FROM [Derailleur];
+SELECT * FROM [Crankset];
+SELECT * FROM [Tyres];
+SELECT * FROM [Chain];
+SELECT * FROM [Brakes];
+SELECT * FROM [Cassette];
+SELECT * FROM [Shifter];
