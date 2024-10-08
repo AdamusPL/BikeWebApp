81b1a61a59167f30be7cc2ad80db95770f1c3655
AdamusPL <adam.czekalski.szczecin@gmail.com>
2024-10-08 16:27:02 +0200

    some changes

--- Database/Queries/0002_FILL_WITH_EXAMPLE_DATA.sql
+++ Database/Queries/0002_FILL_WITH_EXAMPLE_DATA.sql
@@ -2,13 +2,15 @@ USE BikeShop;
 GO
 
 INSERT INTO [UserData] VALUES('Admin', 'Admin');
-INSERT INTO [UserContact] VALUES('admin@admin.pl', '+48123456789', 1);
+INSERT INTO [UserEmail] VALUES('admin@admin.pl', 1);
+INSERT INTO [UserPhoneNumber] VALUES('+48123456789', 1);
 INSERT INTO [User] VALUES('admin', 'admin', 1);
 
 INSERT INTO [ShopAssistant] VALUES(1);
 
 INSERT INTO [UserData] VALUES('Client', 'Example');
-INSERT INTO [UserContact] VALUES('client@gmail.com', '+48123456788', 2);
+INSERT INTO [UserEmail] VALUES('client@test.pl', 2);
+INSERT INTO [UserPhoneNumber] VALUES('+48123456788', 2);
 INSERT INTO [User] VALUES('client', 'example', 2);
 
 INSERT INTO [Client] VALUES(2);
@@ -40,19 +42,58 @@ INSERT INTO [PartType] VALUES('Rear Shifters');
 INSERT INTO [PartType] VALUES('Crankset');
 INSERT INTO [PartType] VALUES('Chain');
 
+INSERT INTO [PartAttribute] VALUES('27.5"', 1);
+INSERT INTO [PartAttribute] VALUES('29"', 1);
+
+INSERT INTO [PartAttribute] VALUES('2 rows', 2);
+INSERT INTO [PartAttribute] VALUES('3 rows', 2);
+
+INSERT INTO [PartAttribute] VALUES('7 rows', 3);
+INSERT INTO [PartAttribute] VALUES('8 rows', 3);
+INSERT INTO [PartAttribute] VALUES('9 rows', 3);
+INSERT INTO [PartAttribute] VALUES('10 rows', 3);
+INSERT INTO [PartAttribute] VALUES('11 rows', 3);
+
 INSERT INTO [PartAttribute] VALUES('V-Brake', 4);
 INSERT INTO [PartAttribute] VALUES('Mechanical', 4);
 INSERT INTO [PartAttribute] VALUES('Hydraulic', 4);
 
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 F', 79.99, 10, 2, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 R', 79.99, 10, 3, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('PFB', 'E-500', 139.99, 2, 4, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 CS', 109.99, 5, 8, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'Bumpy', 159.99, 20, 1, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('ClunkyJ', 'a8', 159.99, 20, 9, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 C', 89.99, 40, 5, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 FS', 159.99, 20, 6, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 RS', 159.99, 20, 7, 1);
+INSERT INTO [PartAttribute] VALUES('7 rows', 5);
+INSERT INTO [PartAttribute] VALUES('8 rows', 5);
+INSERT INTO [PartAttribute] VALUES('9 rows', 5);
+INSERT INTO [PartAttribute] VALUES('10 rows', 5);
+INSERT INTO [PartAttribute] VALUES('11 rows', 5);
+
+INSERT INTO [PartAttribute] VALUES('2 rows', 6);
+INSERT INTO [PartAttribute] VALUES('3 rows', 6);
+
+INSERT INTO [PartAttribute] VALUES('7 rows', 7);
+INSERT INTO [PartAttribute] VALUES('8 rows', 7);
+INSERT INTO [PartAttribute] VALUES('9 rows', 7);
+INSERT INTO [PartAttribute] VALUES('10 rows', 7);
+INSERT INTO [PartAttribute] VALUES('11 rows', 7);
+
+INSERT INTO [PartAttribute] VALUES('1 row', 8);
+INSERT INTO [PartAttribute] VALUES('2 rows', 8);
+INSERT INTO [PartAttribute] VALUES('3 rows', 8);
+
+INSERT INTO [PartAttribute] VALUES('7 rows', 9);
+INSERT INTO [PartAttribute] VALUES('8 rows', 9);
+INSERT INTO [PartAttribute] VALUES('9 rows', 9);
+INSERT INTO [PartAttribute] VALUES('10 rows', 9);
+INSERT INTO [PartAttribute] VALUES('11 rows', 9);
+
+SELECT * FROM [PartAttribute];
+
+INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, PartAttributeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 F', 79.99, 10, 2, 1);
+INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, PartAttributeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 R', 79.99, 10, 3, 1);
+INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, PartAttributeId, ShopAssistantId) VALUES('PFB', 'E-500', 139.99, 2, 4, 1);
+INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, PartAttributeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 CS', 109.99, 5, 8, 1);
+INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, PartAttributeId, ShopAssistantId) VALUES('SwiftZPart', 'Bumpy', 159.99, 20, 1, 1);
+INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, PartAttributeId, ShopAssistantId) VALUES('ClunkyJ', 'a8', 159.99, 20, 9, 1);
+INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, PartAttributeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 C', 89.99, 40, 5, 1);
+INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, PartAttributeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 FS', 159.99, 20, 6, 1);
+INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, PartAttributeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 RS', 159.99, 20, 7, 1);
 
 INSERT INTO [OrderStatus] VALUES('Ordered');
 INSERT INTO [OrderStatus] VALUES('In-Progress');
