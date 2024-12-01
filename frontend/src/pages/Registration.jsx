6995cd92c32aa144105ed80b74efd501cb8d487c
AdamusPL <adam.czekalski.szczecin@gmail.com>
2024-12-01 03:24:38 +0100

    removed nothing meaning constraints

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -53,15 +53,15 @@ export default function Registration() {
                 />
                 <figcaption className='figure-caption d-flex justify-content-center'>Let's start your story with Bike Paradise!</figcaption>
             </figure>
-            <MDBInput label="First Name" id="typeText" type="text" className="mt-5" minLength={2} maxLength={18} pattern="^[A-Za-z]+([-' ][A-Za-z]+)*$" required onChange={(e) => { setFirstName(e.target.value) }} />
-            <MDBInput label="Last Name" id="typeText" type="text" className="mt-3" minLength={2} maxLength={48} pattern="^[A-Za-z]+([-' ][A-Za-z]+)*$" required onChange={(e) => { setLastName(e.target.value) }} />
+            <MDBInput label="First Name" id="typeText" type="text" className="mt-5" onChange={(e) => { setFirstName(e.target.value) }} />
+            <MDBInput label="Last Name" id="typeText" type="text" className="mt-3" onChange={(e) => { setLastName(e.target.value) }} />
 
-            <MDBInput label="Username" id="typeText" type="text" className="mt-5" minLength={6} maxLength={30} pattern="^[A-Za-z0-9_.]+$" required onChange={(e) => { setUsername(e.target.value) }} />
-            <MDBInput label="E-mail Address" id="typeEmail" type="email" className="mt-3" minLength={3} maxLength={64} pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" required onChange={(e) => { setEmail(e.target.value) }} />
-            <MDBInput label="Phone Number" id="typePhone" type="tel" className="mt-3" minLength={9} maxLength={13} pattern="^\+?\d+$" required onChange={(e) => { setPhoneNumber(e.target.value) }} />
+            <MDBInput label="Username" id="typeText" type="text" className="mt-5" onChange={(e) => { setUsername(e.target.value) }} />
+            <MDBInput label="E-mail Address" id="typeEmail" type="email" className="mt-3" required onChange={(e) => { setEmail(e.target.value) }} />
+            <MDBInput label="Phone Number" id="typePhone" type="tel" className="mt-3" onChange={(e) => { setPhoneNumber(e.target.value) }} />
 
-            <MDBInput label="Password" id="typePassword" type="password" className="mt-5" minLength={8} maxLength={128} required onChange={(e) => { setPassword(e.target.value) }} />
-            <MDBInput label="Confirm Password" id="typePassword" type="password" className="mt-3" minLength={8} maxLength={128} required onChange={(e) => { setConfirmedPassword(e.target.value) }} />
+            <MDBInput label="Password" id="typePassword" type="password" className="mt-5" onChange={(e) => { setPassword(e.target.value) }} />
+            <MDBInput label="Confirm Password" id="typePassword" type="password" className="mt-3" onChange={(e) => { setConfirmedPassword(e.target.value) }} />
 
             <MDBContainer className="mt-3">
                 <MDBRadio name='flexRadioDefault' id='client' label='Customer' value={false} onChange={handleRadioButtonChange} defaultChecked />
