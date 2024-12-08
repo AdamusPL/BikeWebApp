9d84d2a06c74407d912d966c11ae5f3e82a34b77
AdamusPL <adam.czekalski.szczecin@gmail.com>
2024-12-08 22:33:25 +0100

    from main

--- Database/Queries/0001_FILL_DICTIONARY_TABLES.sql
+++ Database/Queries/0001_FILL_DICTIONARY_TABLES.sql
@@ -1,10 +1,9 @@
 USE BikeShop;
 GO
 
-SELECT * FROM [User];
-
-SELECT * FROM [BikeParameterAttribute];
-SELECT * FROM [BikeParameterType];
+INSERT INTO [BikeParameterType] VALUES('Type');
+INSERT INTO [BikeParameterType] VALUES('Frame size');
+INSERT INTO [BikeParameterType] VALUES('Make');
 
 INSERT INTO [BikeParameterAttribute] VALUES('MTB', 1);
 INSERT INTO [BikeParameterAttribute] VALUES('BMX', 1);
@@ -21,13 +20,9 @@ INSERT INTO [BikeParameterAttribute] VALUES('L', 2);
 INSERT INTO [BikeParameterAttribute] VALUES('XL', 2);
 INSERT INTO [BikeParameterAttribute] VALUES('XXL', 2);
 
-INSERT INTO [BikeParameterAttribute] VALUES('AeroBike', 3);
-INSERT INTO [BikeParameterAttribute] VALUES('Phumay', 3);
-INSERT INTO [BikeParameterAttribute] VALUES('LizardCycle', 3);
-
-INSERT INTO [BikeParameterType] VALUES('Type');
-INSERT INTO [BikeParameterType] VALUES('Frame size');
-INSERT INTO [BikeParameterType] VALUES('Make');
+INSERT INTO [BikeParameterAttribute] VALUES('Trek', 3);
+INSERT INTO [BikeParameterAttribute] VALUES('Kross', 3);
+INSERT INTO [BikeParameterAttribute] VALUES('Fischer', 3);
 
 INSERT INTO [BikeAttribute] VALUES(1, 1);
 INSERT INTO [BikeAttribute] VALUES(2, 1);
@@ -46,17 +41,15 @@ INSERT INTO [BikeAttribute] VALUES(14, 3);
 INSERT INTO [BikeAttribute] VALUES(15, 3);
 INSERT INTO [BikeAttribute] VALUES(16, 3);
 
-SELECT * FROM [BikeAttribute];
-
-SELECT * FROM [BikeIdentificationAvailable];
-
-INSERT INTO [Bike] (ModelName, Price, Description, ShopAssistantId) VALUES('Swift 4', 1999.00, 'Lorem ipsum', 1);
-INSERT INTO [Bike_BikeParameterType] VALUES(1, 1);
-INSERT INTO [Bike_BikeParameterType] VALUES(10, 1);
-INSERT INTO [Bike_BikeParameterType] VALUES(14, 1);
-INSERT INTO [Bike_BikeParameterAttribute] VALUES(1, 1);
-INSERT INTO [Bike_BikeParameterAttribute] VALUES(10, 1);
-INSERT INTO [Bike_BikeParameterAttribute] VALUES(14, 1);
+INSERT INTO [PartType] VALUES('Tyres');
+INSERT INTO [PartType] VALUES('Front Derailleur');
+INSERT INTO [PartType] VALUES('Rear Derailleur');
+INSERT INTO [PartType] VALUES('Brakes');
+INSERT INTO [PartType] VALUES('Cassette');
+INSERT INTO [PartType] VALUES('Front Shifters');
+INSERT INTO [PartType] VALUES('Rear Shifters');
+INSERT INTO [PartType] VALUES('Crankset');
+INSERT INTO [PartType] VALUES('Chain');
 
 INSERT INTO [PartAttribute] VALUES('27.5"', 1);
 INSERT INTO [PartAttribute] VALUES('29"', 1);
@@ -104,44 +97,7 @@ INSERT INTO [PartAttribute] VALUES('V-Brake', 4);
 INSERT INTO [PartAttribute] VALUES('Mechanical', 4);
 INSERT INTO [PartAttribute] VALUES('Hydraulic', 4);
 
-INSERT INTO [PartType] VALUES('Tyres');
-INSERT INTO [PartType] VALUES('Front Derailleur');
-INSERT INTO [PartType] VALUES('Rear Derailleur');
-INSERT INTO [PartType] VALUES('Brakes');
-INSERT INTO [PartType] VALUES('Cassette');
-INSERT INTO [PartType] VALUES('Front Shifters');
-INSERT INTO [PartType] VALUES('Rear Shifters');
-INSERT INTO [PartType] VALUES('Crankset');
-INSERT INTO [PartType] VALUES('Chain');
-
-SELECT * FROM [PartType];
-SELECT * FROM [PartAttribute];
-
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 F', 79.99, 10, 2, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 R', 79.99, 10, 3, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('PFB', 'E-500', 139.99, 2, 4, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 CS', 109.99, 5, 5, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'Bumpy', 159.99, 20, 1, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('ClunkyJ', 'a8', 159.99, 20, 9, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 C', 89.99, 40, 8, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 FS', 159.99, 20, 6, 1);
-INSERT INTO [Part] (Make, ModelName, Price, QuantityInStock, PartTypeId, ShopAssistantId) VALUES('SwiftZPart', 'E-500 RS', 159.99, 20, 7, 1);
-
 INSERT INTO [OrderStatus] VALUES('Ordered');
 INSERT INTO [OrderStatus] VALUES('In-Progress');
 INSERT INTO [OrderStatus] VALUES('Ready to collect');
 INSERT INTO [OrderStatus] VALUES('Completed');
-
-INSERT INTO [Bike_Part] VALUES(1,1);
-INSERT INTO [Bike_Part] VALUES(2,1);
-INSERT INTO [Bike_Part] VALUES(3,1);
-INSERT INTO [Bike_Part] VALUES(4,1);
-INSERT INTO [Bike_Part] VALUES(5,1);
-INSERT INTO [Bike_Part] VALUES(6,1);
-INSERT INTO [Bike_Part] VALUES(7,1);
-INSERT INTO [Bike_Part] VALUES(8,1);
-INSERT INTO [Bike_Part] VALUES(9,1);
-
-INSERT INTO [BikeIdentificationAvailable] VALUES('1111111111', 1);
-INSERT INTO [BikeIdentificationAvailable] VALUES('1111111112', 1);
-INSERT INTO [BikeIdentificationAvailable] VALUES('1111111113', 1);
